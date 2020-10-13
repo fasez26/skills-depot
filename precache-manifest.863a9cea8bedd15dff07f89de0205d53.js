7f86044fd75ce6dfc9e52d2b6c42d3254dde9921
fasez26 <nallely232@hotmail.com>
2020-10-13 18:30:20 -0500

    Updates

---
+++ precache-manifest.863a9cea8bedd15dff07f89de0205d53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bdaa85e92b3cd20acd848c6270237c1",
+    "url": "/skills-depot/index.html"
+  },
+  {
+    "revision": "c1f44ca8951a51915cd6",
+    "url": "/skills-depot/static/css/main.a53b65d1.chunk.css"
+  },
+  {
+    "revision": "e5db1194955e3e4d71bc",
+    "url": "/skills-depot/static/js/2.aed879a7.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/skills-depot/static/js/2.aed879a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1f44ca8951a51915cd6",
+    "url": "/skills-depot/static/js/main.dc4806b6.chunk.js"
+  },
+  {
+    "revision": "5430d76714104e5eba1c",
+    "url": "/skills-depot/static/js/runtime-main.e8b7d394.js"
+  }
+]);
